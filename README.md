03c89b50dc5ed78344bea2138abb418a4ace53ac
Lukas Hlavacka <lukas@wiredsolutions.ca>
2016-02-03 23:54:59 -0500

    Added support for undecoded BLOBS containing RTF
    Improved status changes. Added support for de-selecting table. Added
    loading message while DB in loaded

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Select or drop SQLite file and UI should update.
 ## TODO
 
 - [ ] Find out why empty tables don't return list of columns
-- [ ] Loading message while large database is working
+- [x] Loading message while large database is working
 - [ ] Investigate use of Web Worked for async use
 
 ## References
--- app.js
+++ app.js
@@ -45,12 +45,41 @@ var Body = React.createClass({
 })
 
 var Row = React.createClass({
+	convertToPlain: function(rtf) {
+	    rtf = rtf.replace(/\\par[d]?/g, "");
+	    return rtf.replace(/\{\*?\\[^{}]+}|[{}]|\\\n?[A-Za-z]+\n?(?:-?\d+)?[ ]?/g, "").trim();
+	},
+	decodeRTF: function(input)
+	{
+		if(input instanceof Uint8Array)
+		{
+			try {
+				var decodedValue = String.fromCharCode.apply(null, input);
+				var textRTFIdentifier = 'textRTF = "';
+				var textRTFIndex = decodedValue.indexOf(textRTFIdentifier);					
+				if(textRTFIndex >= 0)
+				{
+					var endBase64Index = decodedValue.indexOf('"', textRTFIndex + textRTFIdentifier.length);
+					var base64 = decodedValue.substring(textRTFIndex + textRTFIdentifier.length, endBase64Index)
+					var decoded64String = atob(base64);
+
+					return this.convertToPlain(decoded64String);
+				}
+			}
+			catch(err)
+			{
+				return input;
+			}
+		}
+		else
+			return input;
+	},
 	render: function() {
 		return (
 			<tr>
 				{this.props.values.map(function(v, i){
-					return <td key={i}>{ v }</td>
-				}.bind(this))}			
+					return <td key={i}>{ this.decodeRTF(v) }</td>
+				}.bind(this))}
 			</tr>
 		);
 	}
@@ -71,10 +100,7 @@ var BootstrapRow = React.createClass({
 var TableSource = React.createClass({
 	handleChange: function(event)
 	{
-		var table = ReactDOM.findDOMNode(this.refs.table).value
-		if(!table)
-			return;
-
+		var table = ReactDOM.findDOMNode(this.refs.table).value;
 		this.props.handleTableChange(table);
 	},
 	render: function() {
@@ -85,7 +111,7 @@ var TableSource = React.createClass({
 						<label htmlFor="table">Table</label>
 						<select className="form-control" id="table" onChange={this.handleChange} ref="table">
 							<option value="">Select table</option>
-							{this.props.tables.map(function(t){
+							{this.props.tables.map(function(t){								
 								return <option key={t} value={t}>{t}</option>
 							})}
 						</select>
@@ -100,10 +126,17 @@ var FileDropWrapper = React.createClass({
 	onDragEnter : function(event) {
 		event.stopPropagation();
 		event.preventDefault();
+		this.props.handleStatusChange("Drop file anywhere")
+	},
+	onDragLeave : function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		this.props.handleStatusChange("Drag any SQLite file")
 	},
 	onDragOver : function(event) {
 		event.stopPropagation();
 		event.preventDefault();
+		this.props.handleStatusChange("Drop file anywhere")
 	},
 	onDrop: function(event){
 		event.stopPropagation();
@@ -117,7 +150,7 @@ var FileDropWrapper = React.createClass({
 	},
 	render: function() {
 		return (
-			<div style={{ height: "100%" }} onDrop={this.onDrop} onDragEnter={this.onDragEnter} onDragOver={this.onDragOver}>
+			<div style={{ height: "100%" }} onDrop={this.onDrop} onDragEnter={this.onDragEnter} onDragOver={this.onDragOver} onDragLeave={this.onDragLeave}>
 				{this.props.children}
 			</div>
 		);
@@ -136,7 +169,8 @@ var Status = React.createClass({
 
 var Interface = React.createClass({
 	queryData: function(state) {
-		// debugger;
+		if(!state.table)
+			return;
 		var query = "select * from " + state.table;
 
 		if(state.col)
@@ -152,13 +186,11 @@ var Interface = React.createClass({
 		return data[0];
 	},
 	getTables: function(db) {
-		// debugger;
 		var data = db.exec("select name from sqlite_master WHERE type='table'")
 		return data[0].values.map(function(t){ return t[0]});
 	},
 	handleSortChange: function(col) {
-		this.setState(function(previousState){
-			// debugger;			
+		this.setState(function(previousState){		
 			if(previousState.col === col)
 				previousState.sort = !previousState.sort;
 			else
@@ -169,8 +201,7 @@ var Interface = React.createClass({
 			previousState.data = this.queryData(previousState);
 		})
 	},
-	handleTableChange: function(table) {		
-			// debugger;
+	handleTableChange: function(table) {
 		this.setState(function(previousState){
 			previousState.table = table;
 			previousState.data = this.queryData(previousState)
@@ -179,20 +210,18 @@ var Interface = React.createClass({
 	handleDisplayChange: function() {
 		this.setState(function(previousState) {
 			// debugger;
-			if(previousState.table){
-				previousState.data = this.queryData(previousState);
-			}
+			previousState.data = previousState.table && this.queryData(previousState);
 		});
 	},
 	handleFileChange: function(file)
 	{
-		this.state.status = "Loading";
+		this.handleStatusChange("Loading...");
 		var now = new Date();
 		var sqlReader = new FileReader();
 		sqlReader.onload = function() {
 			var Uints = new Uint8Array(sqlReader.result);
 			var db = new SQL.Database(Uints);
-			this.state.status = "Loaded in " + (new Date() - now) + " miliseconds."
+			this.handleStatusChange("Loaded in " + (new Date() - now) + " miliseconds.");
 			this.setState(function(previousState) {
 				previousState.db = db;
 				previousState.tables = this.getTables(db);
@@ -201,25 +230,37 @@ var Interface = React.createClass({
 		}.bind(this);
 		sqlReader.readAsArrayBuffer(file);
 	},
+	handleStatusChange: function(status)
+	{
+		this.setState({ status: status });
+	},
 	getInitialState: function() {
 		return {
 			data: undefined,
 			tables: [],
 			table: undefined,
-			status: "Drop file"
+			status: "Drag any SQLite file"
 		}
 	},
 	render: function() {
+		var tableSource;	
 		var table;
-		if(this.state.data && this.state.data.columns && this.state.data.columns.length)		
-			table = <Table data={this.state.data} handleSortChange={this.handleSortChange}/>
-		else
-			table = <Status status={"No data"} />
+		if(this.state.tables && this.state.tables.length)
+		{
+			tableSource = <TableSource tables={this.state.tables} handleTableChange={this.handleTableChange}/>;		
+
+			if(this.state.data && this.state.data.columns && this.state.data.columns.length)		
+				table = <Table data={this.state.data} handleSortChange={this.handleSortChange}/>
+			else if(this.state.table)
+				table = <Status status={"No data"} />
+			else
+				table = <Status status={"No table selected"} />
+		}
 		
 		return (
-			<FileDropWrapper handleFileChange={this.handleFileChange} >
+			<FileDropWrapper handleFileChange={this.handleFileChange} handleStatusChange={this.handleStatusChange}>
 				<Status status={this.state.status} />
-				<TableSource tables={this.state.tables} handleTableChange={this.handleTableChange}/>
+				{tableSource}
 				{table}
 			</FileDropWrapper>
 		);
